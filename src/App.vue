ec060c5f2f7c48fb05a0f27ebb7775ef303b0b7d
winstonajr <winston.almeidamjr@gmail.com>
2025-07-17 13:03:11 -0300

    frist commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <ConviteCasamento />
+  </div>
+</template>
+
+<script lang="ts">
+import ConviteCasamento from './components/ConviteModerno.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ConviteCasamento
+  }
+}
+</script>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import '@fontsource/playfair-display'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
